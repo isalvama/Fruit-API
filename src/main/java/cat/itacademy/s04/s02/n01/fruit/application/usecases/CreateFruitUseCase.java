ac3b217c3673ce4d0722624174b7ced308114458
isalvama <inesalvans@gmail.com>
2026-06-04 10:03:57 +0200

    refactor: rename createFruitInKg() method to createFruit() and add logic to convert primitive-type params to domain instances

--- src/main/java/cat/itacademy/s04/s02/n01/fruit/application/usecases/CreateFruitUseCase.java
+++ src/main/java/cat/itacademy/s04/s02/n01/fruit/application/usecases/CreateFruitUseCase.java
@@ -1,9 +1,7 @@
 package cat.itacademy.s04.s02.n01.fruit.application.usecases;
-
 import cat.itacademy.s04.s02.n01.fruit.domain.model.Fruit;
-import cat.itacademy.s04.s02.n01.fruit.domain.model.Weight;
 
 public interface CreateFruitUseCase {
 
-    public Fruit createFruitInKg(String name, Weight weight);
+    public Fruit createFruit(String name, double weightAmount, String magnitude);
 }
