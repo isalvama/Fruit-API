d855c2152763ad22654e288c30ab0ac89264b8ca
isalvama <inesalvans@gmail.com>
2026-06-05 18:43:03 +0200

    refactor: add @Size constraints to name attribute of UpdateFruitRequestDTO class, add validateName() method to validate if name is blank and throw a InvalidRequestException if it is blank

--- src/main/java/cat/itacademy/s04/s02/n01/fruit/controller/CreateFruitRequestDTO.java
+++ src/main/java/cat/itacademy/s04/s02/n01/fruit/controller/CreateFruitRequestDTO.java
@@ -2,13 +2,11 @@ package cat.itacademy.s04.s02.n01.fruit.controller;
 
 import cat.itacademy.s04.s02.n01.fruit.controller.exception.InvalidRequestException;
 import cat.itacademy.s04.s02.n01.fruit.domain.model.Magnitude;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.*;
 
 public record CreateFruitRequestDTO(
         @NotNull
-        @NotBlank (message = "Name cannot be blank")
+        @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
         String name,
         @Positive (message = "Weight Amount cannot be negative")
         double weightAmount,
@@ -17,12 +15,19 @@ public record CreateFruitRequestDTO(
 ) {
     public CreateFruitRequestDTO {
         validateMagnitude(magnitude);
+        validateName(name);
     }
 
     private void validateMagnitude(String magnitude) {
         boolean isValid = Magnitude.validateMagnitude(magnitude);
         if (!isValid) {
             throw new InvalidRequestException("Magnitude is not valid");
+        }
+    }
+
+    private void validateName(String name) {
+        if (name.isBlank()){
+            throw new InvalidRequestException("Name can not be blank");
         }
     }
 }
