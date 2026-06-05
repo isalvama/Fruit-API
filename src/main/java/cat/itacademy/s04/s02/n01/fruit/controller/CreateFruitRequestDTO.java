061309ff371d1af2ff8106ab62aad501a9373d9f
isalvama <inesalvans@gmail.com>
2026-06-05 12:54:08 +0200

    format: remove unused imports from CreateFruitRequestDTO class

--- src/main/java/cat/itacademy/s04/s02/n01/fruit/controller/CreateFruitRequestDTO.java
+++ src/main/java/cat/itacademy/s04/s02/n01/fruit/controller/CreateFruitRequestDTO.java
@@ -1,16 +1,10 @@
 package cat.itacademy.s04.s02.n01.fruit.controller;
 
-import cat.itacademy.s04.s02.n01.fruit.controller.exception.FruitNotFoundException;
 import cat.itacademy.s04.s02.n01.fruit.controller.exception.InvalidRequestException;
-import cat.itacademy.s04.s02.n01.fruit.domain.exception.InvalidWeightException;
 import cat.itacademy.s04.s02.n01.fruit.domain.model.Magnitude;
-import cat.itacademy.s04.s02.n01.fruit.domain.model.Weight;
-import ch.qos.logback.classic.spi.IThrowableProxy;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-
-import java.util.Arrays;
 
 public record CreateFruitRequestDTO(
         @NotNull
