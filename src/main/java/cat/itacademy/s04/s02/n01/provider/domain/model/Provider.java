87d5eac181e9412c6cf2b5273af1e57894cf324c
isalvama <inesalvans@gmail.com>
2026-06-07 20:53:33 +0200

    feat: create Provider class domain entity and test its create() factory method in ProviderTest newly created class

---
+++ src/main/java/cat/itacademy/s04/s02/n01/provider/domain/model/Provider.java
@@ -0,0 +1,31 @@
+package cat.itacademy.s04.s02.n01.provider.domain.model;
+
+import cat.itacademy.s04.s02.n01.common.domain.value_object.Name;
+import lombok.Getter;
+import lombok.Setter;
+
+public class Provider {
+    @Getter
+    private final Long id;
+    @Getter
+    @Setter
+    private Name name;
+    @Setter
+    @Getter
+    private Country country;
+
+    public Provider(Long id, Name name, Country country) {
+        this.id = id;
+        this.name = name;
+        this.country = country;
+    }
+
+    public static Provider create(Name name, Country country) {
+        return new Provider(
+                null,
+                name,
+                country
+        );
+    }
+}
+
