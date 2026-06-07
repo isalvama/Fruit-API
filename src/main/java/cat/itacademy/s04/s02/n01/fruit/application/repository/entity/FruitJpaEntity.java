82b720c77a8ad82fc3207aed8fe4fd1c6a843145
isalvama <inesalvans@gmail.com>
2026-06-07 17:55:05 +0200

    config: create Dockerfile and docker-compose.yml files to create image and containers, add @NoArgsConstructor @AllArgsConstructor annotations in FruitJpaEntity class for better performance of the entity, change application.properties and application-test.properties to the new db config, and create application-prod.properties with prod db config parameters

--- src/main/java/cat/itacademy/s04/s02/n01/fruit/application/repository/entity/FruitJpaEntity.java
+++ src/main/java/cat/itacademy/s04/s02/n01/fruit/application/repository/entity/FruitJpaEntity.java
@@ -4,10 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table (name = "fruit")
+@NoArgsConstructor
+@AllArgsConstructor
 public class FruitJpaEntity {
     @Id
     @GeneratedValue
